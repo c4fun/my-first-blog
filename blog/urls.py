1812da8ccfa4decc8325a88ffa6dce5ab39fa876
c4fun <pcfeng502@gmail.com>
2016-02-21 18:32:53 +0700

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # It means that Django will take everything that
     # you place here and transfer it to a view as a variable called pk
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
 
