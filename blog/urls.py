08e6d55b1741a6e6cbf8c0f44a3a9abb13090e59
c4fun <pcfeng502@gmail.com>
2016-02-21 12:35:37 +0800

    Added view and template for detailed blog as weel as CSS

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    # It means that Django will take everything that
+    # you place here and transfer it to a view as a variable called pk
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
+
